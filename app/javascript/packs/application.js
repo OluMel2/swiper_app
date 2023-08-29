5c82aeee672235ca7d56cd1e8263ba8dc43ad600
OluMel2 <129649026+OluMel2@users.noreply.github.com>
2023-08-29 23:58:30 +0000

    13章【RailsにJS,jQueryを実装しよう】

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,21 +16,3 @@ import 'swiper/swiper-bundle.css';
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
-
-
-// Swiperのオプションを定数化
-const opt = {
-  loop: true,
-  pagination: {
-    el: '.swiper-pagination',
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  }
-}
-
-// Swiperを実行(初期化)
-$(document).on('turbolinks:load', function() {
-    let swiper = new Swiper('.swiper',opt);
-});
